cb6c34c68b40c0b6ee8ee22e349a496a53e749bf
Shivkumar Hegonde <shivkumarhegonde@gmail.com>
2022-01-25 15:42:32 +0530

    initial Commit

--- src/main/java/com/audit/spring/boot/microservice/eureka/EurekaServerApp.java
+++ src/main/java/com/audit/spring/boot/microservice/eureka/EurekaServerApp.java
@@ -3,22 +3,13 @@ package com.audit.spring.boot.microservice.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApp.class, args);
-	}
-	
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello Eureka Server Running";
-		
 	}
 
 }
